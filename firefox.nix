1640e8faa584b3d7e9d82d2d978e8721dfe55de9
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2024-08-31 07:42:53 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'firefox-darwin':
        'github:bandithedoge/nixpkgs-firefox-darwin/6cd0ba9bf6c335c99d7c0eb5c8dcd9e458f52875' (2024-07-27)
      → 'github:bandithedoge/nixpkgs-firefox-darwin/cf572d8f6b38f338a534b145082edb7a2178bee2' (2024-08-31)
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/9227223f6d922fee3c7b190b2cc238a99527bbb7' (2024-07-03)
      → 'github:hercules-ci/flake-parts/af510d4a62d071ea13925ce41c95e3dec816c01d' (2024-08-30)
    • Updated input 'flake-parts/nixpkgs-lib':
        'https://github.com/NixOS/nixpkgs/archive/5daf0514482af3f97abaefc78a6606365c9108e2.tar.gz?narHash=sha256-Fm2rDDs86sHy0/1jxTOKB1118Q0O3Uc7EC0iXvXKpbI%3D' (2024-07-01)
      → 'https://github.com/NixOS/nixpkgs/archive/a5d394176e64ab29c852d03346c1fc9b0b7d33eb.tar.gz?narHash=sha256-uFf2QeW7eAHlYXuDktm9c25OxOyCoUOQmh5SZ9amE5Q%3D' (2024-08-01)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/bc2b96acda50229bc99925dde5c8e561e90b0b00' (2024-07-26)
      → 'github:nix-community/home-manager/c2cd2a52e02f1dfa1c88f95abeb89298d46023be' (2024-08-23)
    • Updated input 'nix-darwin':
        'github:lnl7/nix-darwin/72bbc11aedcaba6f9a748786bb0aff9213d8fb36' (2024-07-26)
      → 'github:lnl7/nix-darwin/c8d3157d1f768e382de5526bb38e74d2245cad04' (2024-08-30)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/5ad6a14c6bf098e98800b091668718c336effc95' (2024-07-25)
      → 'github:nixos/nixpkgs/71e91c409d1e654808b2621f28a327acfdad8dc2' (2024-08-28)
    • Updated input 'nur':
        'github:nix-community/NUR/141e2444389e4a387d11768ee110ee5b67b8d1a1' (2024-07-27)
      → 'github:nix-community/NUR/eb369581ee808889f605223c157aa71763307181' (2024-08-31)

--- firefox.nix
+++ firefox.nix
@@ -1,47 +1,82 @@
 {
   pkgs,
   config,
+  lib,
   ...
-}: {
-  programs.firefox = {
+}: let
+  packageSettings = lib.optionalAttrs pkgs.stdenv.isDarwin {
     package = pkgs.firefox-bin;
-    profiles = {
-      ${config.home.username} = {
-        search = {
-          default = "DuckDuckGo";
-          force = true;
-          engines = {
-            "Nix Packages" = {
-              urls = [
-                {
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    {
-                      name = "type";
-                      value = "packages";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
+  };
 
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = ["@np"];
+  cfg = config.programs.firefox;
+in {
+  home.file."Library/Application Support/Firefox/profiles.ini" = let
+    profiles =
+      lib.flip lib.mapAttrs' cfg.profiles (_: profile:
+        lib.nameValuePair "Profile${toString profile.id}" {
+          Name = profile.name;
+          Path =
+            if pkgs.stdenv.isDarwin
+            then "Profiles/${profile.path}"
+            else profile.path;
+          IsRelative = 1;
+          Default =
+            if profile.isDefault
+            then 1
+            else 0;
+        })
+      // {
+        General = {
+          StartWithLastProfile = 1;
+        };
+      };
+
+    profilesIni = lib.generators.toINI {} profiles;
+  in {
+    enable = true;
+    text = lib.mkForce profilesIni;
+  };
+
+  programs.firefox =
+    {
+      profiles = {
+        ${config.home.username} = {
+          search = {
+            default = "DuckDuckGo";
+            force = true;
+            engines = {
+              "Nix Packages" = {
+                urls = [
+                  {
+                    template = "https://search.nixos.org/packages";
+                    params = [
+                      {
+                        name = "type";
+                        value = "packages";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = ["@np"];
+              };
             };
           };
+          extensions = with config.nur.repos.rycee.firefox-addons; [
+            consent-o-matic
+            libredirect
+            ublock-origin
+            vimium
+            stylus
+            bitwarden
+          ];
         };
-        extensions = with config.nur.repos.rycee.firefox-addons; [
-          consent-o-matic
-          libredirect
-          ublock-origin
-          vimium
-          stylus
-          bitwarden
-        ];
       };
-    };
-  };
+    }
+    // packageSettings;
 }
