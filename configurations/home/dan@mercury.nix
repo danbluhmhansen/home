5700f9e2cfaa730d0aced4fda86ce59b5bf18c3c
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2024-10-08 17:39:02 +0200

    - moved gpg-agent to dan@mercury so it is no longer default - enabled darkman service to switch themes for linux

--- configurations/home/dan@mercury.nix
+++ configurations/home/dan@mercury.nix
@@ -4,11 +4,37 @@ in {
   imports = [
     self.homeModules.default
     self.homeModules.linux
-    {
-      home.file = {
-        ".config/wezterm/wezterm.lua".text = "local wezterm = require 'wezterm'\n" + builtins.readFile ../../modules/home/wezterm/wezterm.lua;
-      };
-    }
+    (
+      {pkgs, ...}: {
+        home.file = {
+          ".config/wezterm/wezterm.lua".text = "local wezterm = require 'wezterm'\n" + builtins.readFile ../../modules/home/wezterm/wezterm.lua;
+        };
+
+        services.gpg-agent.enable = true;
+        services.gpg-agent.enableSshSupport = true;
+        services.gpg-agent.defaultCacheTtl = 60;
+        services.gpg-agent.maxCacheTtl = 120;
+        services.gpg-agent.pinentryPackage = pkgs.pinentry-gnome3;
+
+        services.darkman.enable = true;
+        services.darkman.darkModeScripts = {
+          helix = pkgs.writers.writePython3 "helix-dark" {} ''
+            import os
+            with open(os.environ['HOME'] + '/.config/helix/themes/theme.toml', 'w') as file:
+              file.write('inherits = "catppuccin_mocha"')
+            os.system('pkill -USR1 hx')
+          '';
+        };
+        services.darkman.lightModeScripts = {
+          helix = pkgs.writers.writePython3 "helix-light" {} ''
+            import os
+            with open(os.environ['HOME'] + '/.config/helix/themes/theme.toml', 'w') as file:
+              file.write('inherits = "catppuccin_latte"')
+            os.system('pkill -USR1 hx')
+          '';
+        };
+      }
+    )
     ../../modules/home/starship.nix
   ];
   home.username = "dan";
--- modules/home/linux.nix
+++ modules/home/linux.nix
@@ -1,15 +1,7 @@
 {
   imports = [
-    (
-      {pkgs, ...}: {
-        programs.bash.enable = true;
-
-        services.gpg-agent.enable = true;
-        services.gpg-agent.enableSshSupport = true;
-        services.gpg-agent.defaultCacheTtl = 60;
-        services.gpg-agent.maxCacheTtl = 120;
-        services.gpg-agent.pinentryPackage = pkgs.pinentry-gnome3;
-      }
-    )
+    {
+      programs.bash.enable = true;
+    }
   ];
 }
