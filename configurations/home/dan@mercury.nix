9606074f93690d6e6f6792babfc74bafe5cfa6fd
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2025-03-16 19:32:22 +0100

    fixed rebase

--- configurations/home/dan@mercury.nix
+++ configurations/home/dan@mercury.nix
@@ -9,22 +9,6 @@ in {
   imports = [
     self.homeModules.default
     self.homeModules.linux
-    (
-      {pkgs, ...}: {
-        home.file = {
-          ".config/wezterm/wezterm.lua".text = "local wezterm = require 'wezterm'\n" + builtins.readFile ../../modules/home/wezterm/wezterm.lua;
-        };
-
-        programs.mpv.enable = true;
-        programs.yt-dlp.enable = true;
-
-        services.gpg-agent.enable = true;
-        services.gpg-agent.enableSshSupport = true;
-        services.gpg-agent.defaultCacheTtl = 60;
-        services.gpg-agent.maxCacheTtl = 120;
-        services.gpg-agent.pinentryPackage = pkgs.pinentry-gnome3;
-      }
-    )
     ../../modules/home/starship.nix
   ];
   home.username = "dan";
@@ -36,6 +20,8 @@ in {
 
   programs.helix.package = inputs.helix.packages.${pkgs.system}.default;
   programs.yazi.package = inputs.yazi.packages.${pkgs.system}.default;
+  programs.mpv.enable = true;
+  programs.yt-dlp.enable = true;
 
   services.gpg-agent.enable = true;
   services.gpg-agent.enableSshSupport = true;
