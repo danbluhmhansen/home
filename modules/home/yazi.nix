ed296ad9f9f37994482165702cc69dd2d73777ae
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2025-03-22 13:07:02 +0100

    idiomatic yazi plugin config

--- modules/home/yazi.nix
+++ modules/home/yazi.nix
@@ -1,17 +1,68 @@
-{pkgs, ...}: {
-  home.file = {
-    ".config/yazi/plugins/smart-filter.yazi/main.lua".source = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/yazi-rs/plugins/0b9f325fe9d1edbc6d7893344cd308533ebd827a/smart-filter.yazi/init.lua";
-      hash = "sha256-2iB0ADNf+oT4KJPQhXOo2McydZj4YL+I3jFUlkgjFc0=";
-    };
+{pkgs, ...}: let
+  yazi-plugins = pkgs.fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "plugins";
+    rev = "273019910c1111a388dd20e057606016f4bd0d17";
+    hash = "sha256-80mR86UWgD11XuzpVNn56fmGRkvj0af2cFaZkU8M31I=";
   };
+in {
   programs.yazi.enable = true;
+  programs.yazi.plugins = {
+    diff = "${yazi-plugins}/diff.yazi";
+    git = "${yazi-plugins}/git.yazi";
+    jump-to-char = "${yazi-plugins}/jump-to-char.yazi";
+    mount = "${yazi-plugins}/mount.yazi";
+    smart-enter = "${yazi-plugins}/smart-enter.yazi";
+    smart-filter = "${yazi-plugins}/smart-filter.yazi";
+    vcs-files = "${yazi-plugins}/vcs-files.yazi";
+  };
+  programs.yazi.initLua = ''
+    require("git"):setup()
+  '';
+  programs.yazi.settings = {
+    plugin.prepend_fetchers = [
+      {
+        id = "git";
+        name = "*";
+        run = "git";
+      }
+      {
+        id = "git";
+        name = "*/";
+        run = "git";
+      }
+    ];
+  };
   programs.yazi.keymap = {
     manager.prepend_keymap = [
+      {
+        on = "<C-d>";
+        run = "plugin diff";
+        desc = "Diff the selected with the hovered file";
+      }
+      {
+        on = "f";
+        run = "plugin jump-to-charr";
+        desc = "Jump to char";
+      }
+      {
+        on = "M";
+        run = "plugin mount";
+      }
+      {
+        on = "l";
+        run = "plugin smart-enter";
+        desc = "Enter the child directory, or open the file";
+      }
       {
         on = "F";
         run = "plugin smart-filter";
         desc = "Smart filter";
+      }
+      {
+        on = ["g" "c"];
+        run = "plugin vcs-files";
+        desc = "Show Git file changes";
       }
     ];
   };
