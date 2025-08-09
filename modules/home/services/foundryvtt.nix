1ad04c1224343ec3a665811697fcaea0e810292a
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2025-08-09 22:47:46 +0200

    protect foundry with authelia

--- modules/home/services/foundryvtt.nix
+++ modules/home/services/foundryvtt.nix
@@ -11,6 +11,10 @@
       USER_UID = "1000";
       USER_GID = "1000";
       TZ = timeZone;
+      FOUNDRY_HOSTNAME = "foundry.920301.xyz";
+      FOUNDRY_MINIFY_STATIC_FILES = "true";
+      FOUNDRY_PROXY_SSL = "true";
+      FOUNDRY_PROXY_PORT = "443";
     };
     environmentFile = [config.sops.secrets.foundryvtt.path];
     network = ["traefik"];
@@ -20,6 +24,7 @@
     ];
     labels = {
       "traefik.http.routers.foundryvtt.rule" = ''Host(`foundry.920301.xyz`)'';
+      "traefik.http.routers.foundryvtt.middlewares" = "authelia@docker";
       "traefik.http.services.foundryvtt.loadbalancer.server.port" = "30000";
       "glance.name" = "FoundryVTT";
       "glance.icon" = "si:foundryvirtualtabletop";
