224afc0b3b1946a134884427325a66cde78a74de
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2025-10-24 19:01:53 +0200

    gameyfin container

--- modules/home/saturn.nix
+++ modules/home/saturn.nix
@@ -7,6 +7,7 @@
     ./services/lldap.nix
     ./services/forgejo.nix
     ./services/foundryvtt.nix
+    ./services/gameyfin.nix
     ./services/metube.nix
     ./services/minio.nix
     ./services/postgres.nix
