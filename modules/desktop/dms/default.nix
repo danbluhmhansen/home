c3c059e5553918debba410bf3b64b207f4b636f3
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2026-04-12 10:39:52 +0200

    feat(dms): Track DMS settings

---
+++ modules/desktop/dms/default.nix
@@ -0,0 +1,39 @@
+{inputs, ...}: {
+  flake.modules.nixos.dms = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+      accountsservice
+      adw-gtk3
+      catppuccin-cursors.mochaDark
+      kdePackages.breeze-icons
+    ];
+    hardware.i2c.enable = true;
+    programs.dms-shell = {
+      enable = true;
+      quickshell.package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
+    };
+    programs.dsearch.enable = true;
+    services.displayManager.dms-greeter = {
+      enable = true;
+      compositor.name = "niri";
+      logs.save = true;
+    };
+    xdg.icons.fallbackCursorThemes = ["catppuccin-mocha-dark-cursors"];
+  };
+  flake.modules.nixos.dms-dan = {config, ...}: {
+    environment.sessionVariables.TERMINAL = "ghostty";
+    services.displayManager.dms-greeter.configHome = config.users.users.dan.home;
+  };
+
+  flake.modules.homeManager.dms = {
+    config,
+    pkgs,
+    ...
+  }: {
+    home.file = {
+      ".config/DankMaterialShell/settings.json".source =
+        config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/.config/home/modules/desktop/dms/settings.json";
+      ".local/share/icons/default/cursors".source = "${pkgs.catppuccin-cursors.mochaDark}/share/icons/catppuccin-mocha-dark-cursors/cursors";
+    };
+  };
+}
