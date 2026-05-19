c8b8a5320b21c139f8b1c14387df57b936fdd92d
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2026-05-19 07:13:00 +0200

    feat(steam): steam controller support

--- modules/apps/steam.nix
+++ modules/apps/steam.nix
@@ -1,11 +1,17 @@
 {
-  flake.modules.nixos.steam = {
-    programs.gamescope.enable = true;
-    programs.gamescope.capSysNice = true;
-    programs.gamescope.args = ["--adaptive-sync" "--hdr-enabled" "--rt"];
-    programs.steam.enable = true;
-    programs.steam.gamescopeSession.enable = true;
-    programs.steam.gamescopeSession.steamArgs = ["-tenfoot" "-pipewire-dmabuf"];
+  flake.modules.nixos.steam = {pkgs, ...}: {
+    hardware.steam-hardware.enable = true;
+    programs.gamescope = {
+      enable = true;
+      capSysNice = true;
+      args = ["--adaptive-sync" "--hdr-enabled" "--rt"];
+    };
+    programs.steam = {
+      enable = true;
+      extraPackages = [pkgs.hidapi];
+      gamescopeSession.enable = true;
+      gamescopeSession.steamArgs = ["-tenfoot" "-pipewire-dmabuf"];
+    };
   };
   flake.modules.darwin.steam.homebrew.casks = [
     {
